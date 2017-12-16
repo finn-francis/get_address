8e98cfe5068604d4acc82f1c0544df4cfd35e690
finn-francis <finn_francis@yahoo.com>
2017-12-16 15:17:21 +0000

    Created the request class

---
+++ lib/get_address/request.rb
@@ -0,0 +1,28 @@
+module GetAddress
+  class Request
+    PERMITTED_VALUES = [:api_key, :format_array, :sort, :postcode, :house, :config].freeze
+
+    attr_accessor *PERMITTED_VALUES
+
+    def initialize(options = {})
+      set_options(options)
+    end
+
+    def config
+      GetAddress.config
+    end
+
+    private
+
+    def set_options(options)
+      { **config_settings, **options }.each do |key, value|
+        next unless PERMITTED_VALUES.include?(key)
+        instance_variable_set "@#{key}", value
+      end
+    end
+
+    def config_settings
+      config.settings
+    end
+  end
+end
---
+++ spec/request_spec.rb
@@ -0,0 +1,40 @@
+require "spec_helper"
+
+RSpec.describe GetAddress::Request do
+  extend SetupConfiguration
+
+  let(:request) { GetAddress::Request.new(options) }
+  let(:options) { {} }
+  before { configure }
+
+  describe "#config" do
+    it "should return the GetAddress config" do
+      expect(request.config).to eq config
+    end
+  end
+
+  describe "#config_settings" do
+    it "should return the config settings" do
+      expect(request.send(:config_settings)).to eq config.settings
+    end
+  end
+
+  describe "#set_options" do
+    let(:options) do
+      {
+        postcode: "FOO",
+        house:    "BAR",
+        api_key:  "OVERRIDDEN_API_KEY"
+      }
+    end
+    let(:set_options) { request.send(:set_options, options) }
+
+    it "should set the passed_in instance variables along with the defaults from the config settings" do
+      expect(set_options[:postcode]).to     eq "FOO"
+      expect(set_options[:house]).to        eq "BAR"
+      expect(set_options[:api_key]).to      eq "OVERRIDDEN_API_KEY"
+      expect(set_options[:sort]).to         eq false
+      expect(set_options[:format_array]).to eq true
+    end
+  end
+end
