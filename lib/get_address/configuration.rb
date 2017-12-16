c9fa82f40d2091a4f4484d9921adf75a6081d9f9
finn-francis <finn_francis@yahoo.com>
2017-12-16 23:31:25 +0000

    Added key_mapping to the configuration

--- lib/get_address/configuration.rb
+++ lib/get_address/configuration.rb
@@ -7,11 +7,35 @@ module GetAddress
       @sort         = true
     end
 
+    def keymap=(mappings)
+      mappings.each do |old_mapping, new_mapping|
+        keymappings[new_mapping] = keymappings.delete(old_mapping)
+      end
+    end
+
     def settings
       {
         api_key:      api_key,
         format_array: format_array,
         sort:         sort
+      }
+    end
+
+    def keymap
+      keymappings.sort_by {|k, v| v}.map(&:first)
+    end
+
+    private
+
+    def keymappings
+      @keymap ||= {
+        line_1:   0,
+        line_2:   1,
+        line_3:   2,
+        line_4:   3,
+        locality: 4,
+        town:     5,
+        country:  6
       }
     end
   end
--- spec/configuration_spec.rb
+++ spec/configuration_spec.rb
@@ -8,10 +8,37 @@ RSpec.describe GetAddress::Configuration do
     let(:config)     { GetAddress::Configuration.new }
     let(:attributes) { [:api_key, :format_array, :sort] }
 
-    it "should have getters and setters for all attributes" do
+    it "should have getters and setters for api_key, format_array and sort" do
       attributes.each do |attribute|
         expect(config).to respond_to(attribute)
         expect(config).to respond_to("#{attribute}=".to_sym)
+      end
+    end
+
+    describe "#keymap" do
+      it "should return the key mappings" do
+        expect(config.keymap).to eq [:line_1, :line_2, :line_3, :line_4, :locality, :town, :country]
+      end
+    end
+
+    describe "#keymap=" do
+      let(:keymap) { { locality: :county } }
+      let(:expected_keymappings) do
+        {
+          line_1:  0,
+          line_2:  1,
+          line_3:  2,
+          line_4:  3,
+          county:  4,
+          town:    5,
+          country: 6
+        }
+      end
+      before { config.keymap = keymap }
+
+      it "should set the keymap and keymappings" do
+        expect(config.keymap).to eq [:line_1, :line_2, :line_3, :line_4, :county, :town, :country]
+        expect(config.send(:keymappings)).to eq expected_keymappings
       end
     end
   end
@@ -28,6 +55,23 @@ RSpec.describe GetAddress::Configuration do
 
     it "should return a hash of the configuration settings" do
       expect(config.settings).to eq(expected_settings)
+    end
+  end
+
+  describe "#keymappings" do
+    let(:keymappings) do
+      {
+        line_1:   0,
+        line_2:   1,
+        line_3:   2,
+        line_4:   3,
+        locality: 4,
+        town:     5,
+        country:  6
+      }
+    end
+    it "should return the keymappings" do
+      expect(config.send(:keymappings)).to eq keymappings
     end
   end
 end
