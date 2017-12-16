3ea8c2bfd38b43ae61c1fcdf825c28277d091acd
finn-francis <finn_francis@yahoo.com>
2017-12-16 15:59:49 +0000

    Include UrlGenerator in request

--- lib/get_address/request.rb
+++ lib/get_address/request.rb
@@ -1,11 +1,14 @@
 module GetAddress
   class Request
+    include GetAddress::UrlGenerator
+
     PERMITTED_VALUES = [:api_key, :format_array, :sort, :postcode, :house].freeze
 
     attr_accessor *PERMITTED_VALUES
 
     def initialize(options = {})
       set_options(options)
+      generate_url(postcode, house)
     end
 
     private
