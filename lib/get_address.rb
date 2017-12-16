45aad9361f5f84babadb7fcdd4b1ea337c8d16e7
finn-francis <finn_francis@yahoo.com>
2017-12-16 15:16:36 +0000

    Added code to setup the GetAddress. Configuration

--- lib/get_address.rb
+++ lib/get_address.rb
@@ -1,5 +1,16 @@
 require "get_address/version"
+require 'get_address/request'
 
 module GetAddress
-  # Your code goes here...
+  class << self
+    def configure
+      @@config = Configuration.new.tap { |config| yield config }
+    end
+
+    def config
+      @@config
+    end
+    # def configuration
+    alias_method :configuration, :config
+  end
 end
--- spec/get_address_spec.rb
+++ spec/get_address_spec.rb
@@ -1,9 +1,47 @@
 RSpec.describe GetAddress do
+  extend SetupConfiguration
+
+  shared_examples "configuration" do
+    it "should return the get_address configuration" do
+      expect(config.api_key).to eq "MY_API_KEY"
+      expect(config.format_array).to be true
+      expect(config.sort).to be false
+    end
+  end
+
   it "has a version number" do
     expect(GetAddress::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe "#configure" do
+    it "should allow configuration options to be set" do
+      expect(configure).to be_truthy
+    end
+  end
+
+  describe "#config" do
+    let(:config) do
+      configure
+      GetAddress.config
+    end
+    it_behaves_like "configuration"
+
+    describe "#configuration(alias)" do
+      let(:config) do
+        configure
+        GetAddress.configuration
+      end
+      it_behaves_like "configuration"
+    end
+
+    describe 'defaults' do
+      let(:configure) { GetAddress.configure { |config| config.api_key = "MY_API_KEY" } }
+
+      it "should set defaults if no arguments are provided" do
+        expect(config.api_key).to eq "MY_API_KEY"
+        expect(config.format_array).to be false
+        expect(config.sort).to be true
+      end
+    end
   end
 end
