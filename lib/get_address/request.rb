c9c49d2b18474feedb1e1cf8400a54742abd802c
finn-francis <finn_francis@yahoo.com>
2017-12-17 15:45:00 +0000

    Added some validity checks to request.rb

--- lib/get_address/request.rb
+++ lib/get_address/request.rb
@@ -8,6 +8,7 @@ module GetAddress
     include GetAddress::ConfigMethods
 
     PERMITTED_VALUES = [:api_key, :format_array, :sort, :postcode, :house].freeze
+    REQUIRED_FIELDS  = [:api_key, :format_array, :sort, :postcode].freeze
 
     attr_reader *PERMITTED_VALUES
 
@@ -19,8 +20,34 @@ module GetAddress
       generate_url
     end
 
+    # validity and error handling
+    # TODO maybe turn into a module later?
+    def valid?
+      errors = []
+      add_to_errors(:missing_fields).nil?
+    end
+
+    def errors
+      @errors ||= []
+    end
+
+    def missing_fields
+      # TODO Add a message if sort or format_array is missing
+      # first tell them to check that they didn't set either of them to nil
+      # if the didn't tell them to submit an issue on github
+      REQUIRED_FIELDS.each_with_object([]) do |field, list|
+        list << field if send(field).nil?
+      end
+    end
+
     private
     attr_reader :options
+    attr_writer :errors
+
+    def add_to_errors(method, *args)
+      result = send(method, *args)
+      errors << { method => result } unless result.nil? || result.empty?
+    end
 
     # TODO turn this into a module later?
     def klass
--- spec/request_spec.rb
+++ spec/request_spec.rb
@@ -7,6 +7,27 @@ RSpec.describe GetAddress::Request do
   let(:options) { { postcode: 'FOO' } }
   before { configure }
 
+  shared_context 'there are missing fields' do
+    before do
+      request.instance_variable_set '@api_key', nil
+      request.valid?
+    end
+
+    let(:missing_api_key) { [{ missing_fields: [:api_key] }] }
+  end
+
+  shared_examples 'it is valid' do
+    it 'should return true' do
+      expect(request.valid?).to be true
+    end
+  end
+
+  shared_examples 'it is not valid' do
+    it 'should return false' do
+      expect(request.valid?).to be false
+    end
+  end
+
   it 'should require a postcode' do
     expect { GetAddress::Request.new({}) }.to raise_error(ArgumentError).with_message('Missing keyword argument: :postcode')
   end
@@ -53,6 +74,72 @@ RSpec.describe GetAddress::Request do
   describe '#klass' do
     it 'should return the class' do
       expect(request.send(:klass)).to be GetAddress::Request
+    end
+  end
+
+  describe '#valid?' do
+    context 'it is valid' do
+      it_behaves_like 'it is valid'
+    end
+
+    context 'it is not valid' do
+      include_context 'there are missing fields'
+      it_behaves_like 'it is not valid'
+
+      it 'should return false' do
+        expect(request.errors).to eq missing_api_key
+      end
+    end
+  end
+
+  describe '#errors' do
+    context 'there are errors' do
+      include_context 'there are missing fields'
+      it_behaves_like 'it is not valid'
+
+      it 'should return a list of all the errors' do
+        expect(request.errors).to eq missing_api_key
+      end
+    end
+
+    context 'there are no errors' do
+      it_behaves_like 'it is valid'
+      it 'should return an empty array' do
+        expect(request.errors).to match_array []
+      end
+    end
+  end
+
+  describe '#missing_fields' do
+    context 'no missing fields' do
+      it 'should return an empty array' do
+        expect(request.missing_fields).to eq []
+      end
+    end
+
+    context 'missing fields' do
+      include_context 'there are missing fields'
+
+      it 'should return all missing fields' do
+        expect(request.missing_fields).to eq [:api_key]
+      end
+    end
+  end
+
+  describe '#add_to_errors' do
+    context 'there are errors' do
+      let(:missing_api_key) { [{ missing_fields: [:api_key] }] }
+      before { request.instance_variable_set '@api_key', nil }
+
+      it 'should add an error to the errors array' do
+        expect { request.send(:add_to_errors, :missing_fields) }.to change { request.errors }.to missing_api_key
+      end
+    end
+
+    context 'there are no errors' do
+      it 'should add an error to the errors array' do
+        expect { request.send(:add_to_errors, :missing_fields) }.to_not change { request.errors }
+      end
     end
   end
 end
