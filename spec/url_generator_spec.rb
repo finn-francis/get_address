1619e3154c8a240788490bdf858ae540f3c800d2
finn-francis <finn_francis@yahoo.com>
2017-12-20 16:37:16 +0000

    Fixed spec to work with the api-key parameter

--- spec/url_generator_spec.rb
+++ spec/url_generator_spec.rb
@@ -2,7 +2,12 @@ require 'spec_helper'
 
 class Test
   include GetAddress::UrlGenerator
-  attr_accessor :postcode, :house
+  attr_accessor :postcode, :house, :api_key
+
+  def initialize
+    @postcode = 'POSTCODE'
+    @api_key  = 'MY_API_KEY'
+  end
 end
 
 RSpec.describe GetAddress::UrlGenerator do
@@ -10,23 +15,19 @@ RSpec.describe GetAddress::UrlGenerator do
   let(:generate_url) { generator.generate_url }
 
   describe '#generate_url' do
-    before do
-      generator.instance_variable_set '@postcode', 'POSTCODE'
-    end
-
     context 'with postcode and house' do
       before do
         generator.instance_variable_set '@house', 'HOUSE'
       end
 
       it 'should return a url with a postcode and house' do
-        expect(generate_url).to eq GetAddress::BASE_URL + 'POSTCODE/HOUSE'
+        expect(generate_url).to eq GetAddress::BASE_URL + 'POSTCODE/HOUSE' + '?api-key=MY_API_KEY'
       end
     end
 
     context 'with just a postcode' do
       it 'should return a url with just the postcode' do
-        expect(generator.generate_url).to eq GetAddress::BASE_URL + 'POSTCODE'
+        expect(generator.generate_url).to eq GetAddress::BASE_URL + 'POSTCODE' + '?api-key=MY_API_KEY'
       end
     end
   end
