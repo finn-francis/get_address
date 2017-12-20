fb7f20890c724cee29bc4d9195e403a703bd1e3c
finn-francis <finn_francis@yahoo.com>
2017-12-20 17:00:43 +0000

    Added TODO for the url generator

--- lib/get_address/url_generator.rb
+++ lib/get_address/url_generator.rb
@@ -3,6 +3,7 @@ module GetAddress
     attr_reader :url
 
     def generate_url
+      # TODO add sort and format_array to the query
       @url = "#{GetAddress::BASE_URL}#{postcode}#{house ? "/#{house}" : "" }?api-key=#{api_key}"
     end
   end
